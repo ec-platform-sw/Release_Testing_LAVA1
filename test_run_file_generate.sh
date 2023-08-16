0ff137fef3b0b0810b992748046ed3b37cad92ee
koteswararao-nayudu <91471931+koteswararao-nayudu@users.noreply.github.com>
2023-08-16 15:18:19 +0530

    Add files via upload

---
+++ test_run_file_generate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#!/bin/sh
+
+cp test_run_generic.yaml test_run.yaml
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+   
+   echo "$KEY"="$VALUE"
+   sed -i "s|$KEY|$VALUE|g" ./test_run.yaml
+done
+
