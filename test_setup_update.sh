3c59dbee426def9645dda5526aca10ff09d90740
koteswararao-nayudu <91471931+koteswararao-nayudu@users.noreply.github.com>
2023-08-26 02:28:00 +0530

    Add files via upload

---
+++ test_setup_update.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   echo "$KEY"="$VALUE"
+   export $KEY=$VALUE
+done
+
+export board_number=${BOARD_TAG:5}
+
+cp /lab-nfs/koti/lava-apc/power_off_b0_titan_generic.sh /lab-nfs/koti/lava-apc/power_off_b0_titan_$board_number.sh
+cp /lab-nfs/koti/lava-apc/power_on_b0_titan_generic.sh /lab-nfs/koti/lava-apc/power_on_b0_titan_$board_number.sh
+cp /lab-nfs/koti/lava-apc/power_reset_bootns_titan_generic.sh /lab-nfs/koti/lava-apc/power_reset_bootns_titan_$board_number.sh
+cp /lab-nfs/koti/lava-apc/boots_b0_titan_generic.sh /lab-nfs/koti/lava-apc/boots_b0_titan_$board_number.sh
+cp /lab-nfs/koti/lava-apc/bootns_b0_titan_generic.sh /lab-nfs/koti/lava-apc/bootns_b0_titan_$board_number.sh
+cp /lab-nfs/koti/lava-apc/flash_b0_titan_generic.sh /lab-nfs/koti/lava-apc/flash_b0_titan_$board_number.sh
+
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   echo "$KEY"="$VALUE"
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_off_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_on_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_reset_bootns_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/boots_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/bootns_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/flash_b0_titan_$board_number.sh
+done
+
+
