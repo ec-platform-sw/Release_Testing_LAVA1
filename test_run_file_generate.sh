97201dfdd40fc886c7103ec44894f8236091b32c
koteswararao-nayudu <91471931+koteswararao-nayudu@users.noreply.github.com>
2023-10-05 14:58:58 +0530

    Update test_run_file_generate.sh

--- test_run_file_generate.sh
+++ test_run_file_generate.sh
@@ -11,6 +11,9 @@ do
    echo "$KEY"="$VALUE"
    sed -i "s|$KEY|$VALUE|g" ./test_run.yaml
 done
-
+MAC_ADDRESS_VALUE=${MAC_ADDRESS}
+MAC_ADDRESS_VALUE=$(echo $MAC_ADDRESS_VALUE | tr '[:lower:]' '[:upper:]')
+MAC_LOWER_ADDRESS=$(echo $MAC_ADDRESS_VALUE | tr '[:upper:]' '[:lower:]')
+sed -i "s|MAC_LOWER_ADDRESS|$MAC_LOWER_ADDRESS|g" ./test_run.yaml
 chmod 777 ./test_run.yaml
 
