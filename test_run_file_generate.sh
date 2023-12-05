dc9853cddf37b75510f1f721856ae237fc20003a
koteswararao-nayudu <91471931+koteswararao-nayudu@users.noreply.github.com>
2023-12-05 23:37:04 +0530

    Update test_run_file_generate.sh

--- test_run_file_generate.sh
+++ test_run_file_generate.sh
@@ -16,7 +16,6 @@ MAC_ADDRESS_VALUE=${MAC_ADDRESS}
 MAC_ADDRESS_VALUE=$(echo $MAC_ADDRESS_VALUE | tr '[:lower:]' '[:upper:]')
 MAC_LOWER_ADDRESS=$(echo $MAC_ADDRESS_VALUE | tr '[:upper:]' '[:lower:]')
 MACADDRESS_VALUE=$(echo $MAC_ADDRESS_VALUE | tr '[:lower:]' '[:upper:]')
-cd /home/equser/koti/platform_builds_trigger
 sed -i "s|MAC_LOWER_ADDRESS|$MAC_LOWER_ADDRESS|g" ./test_run.yaml
 sed -i "s|MACADDRESS_VALUE|$MACADDRESS_VALUE|g" ./test_run.yaml
 chmod 777 ./test_run.yaml
