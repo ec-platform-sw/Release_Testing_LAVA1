2dd3407f58056c51423338a446bcb93e01e9b1aa
koteswararao-nayudu <91471931+koteswararao-nayudu@users.noreply.github.com>
2023-12-05 23:25:07 +0530

    Update test_setup_update.sh

--- test_setup_update.sh
+++ test_setup_update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#exit 1
+set -x
+
 for ARGUMENT in "$@"
 do
    KEY=$(echo $ARGUMENT | cut -f1 -d=)
@@ -15,19 +18,19 @@ export board_number=${BOARD_TAG:5}
 
 if [[ $board_number =~ ^1 ]]
 then
-cp /lab-nfs/koti/lava-apc/power_off_b0_titan_generic.sh /lab-nfs/koti/lava-apc/power_off_b0_titan_$board_number.sh
-cp /lab-nfs/koti/lava-apc/power_on_b0_titan_generic.sh /lab-nfs/koti/lava-apc/power_on_b0_titan_$board_number.sh
-cp /lab-nfs/koti/lava-apc/power_reset_bootns_titan_generic.sh /lab-nfs/koti/lava-apc/power_reset_bootns_titan_$board_number.sh
-cp /lab-nfs/koti/lava-apc/boots_b0_titan_generic.sh /lab-nfs/koti/lava-apc/boots_b0_titan_$board_number.sh
-cp /lab-nfs/koti/lava-apc/bootns_b0_titan_generic.sh /lab-nfs/koti/lava-apc/bootns_b0_titan_$board_number.sh
-cp /lab-nfs/koti/lava-apc/flash_b0_titan_generic.sh /lab-nfs/koti/lava-apc/flash_b0_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/power_off_b0_titan_generic.sh /lava/edgeq/lava-apc/power_off_b0_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/power_on_b0_titan_generic.sh /lava/edgeq/lava-apc/power_on_b0_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/power_reset_bootns_titan_generic.sh /lava/edgeq/lava-apc/power_reset_bootns_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/boots_b0_titan_generic.sh /lava/edgeq/lava-apc/boots_b0_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/bootns_b0_titan_generic.sh /lava/edgeq/lava-apc/bootns_b0_titan_$board_number.sh
+cp /lava/edgeq/lava-apc/flash_b0_titan_generic.sh /lava/edgeq/lava-apc/flash_b0_titan_$board_number.sh
 
-#chmod 777 /lab-nfs/koti/lava-apc/power_off*
-#chmod 777 /lab-nfs/koti/lava-apc/power_on*
-chmod 777 /lab-nfs/koti/lava-apc/power_*
-chmod 777 /lab-nfs/koti/lava-apc/boots_*
-chmod 777 lab-nfs/koti/lava-apc/bootns_*
-chmod 777 /lab-nfs/koti/lava-apc/flash_*
+#chmod 777 /lava/edgeq/lava-apc/power_off*
+#chmod 777 /lava/edgeq/lava-apc/power_on*
+chmod 777 /lava/edgeq/lava-apc/power_*
+chmod 777 /lava/edgeq/lava-apc/boots_*
+chmod 777 /lava/edgeq/lava-apc/bootns_*
+chmod 777 /lava/edgeq/lava-apc/flash_*
 
 for ARGUMENT in "$@"
 do
@@ -37,32 +40,32 @@ do
    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
 
    echo "$KEY"="$VALUE"
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_off_b0_titan_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_on_b0_titan_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/power_reset_bootns_titan_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/boots_b0_titan_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/bootns_b0_titan_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/flash_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/power_off_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/power_on_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/power_reset_bootns_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/boots_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/bootns_b0_titan_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/flash_b0_titan_$board_number.sh
 done
 
-sed -i "s|board_number|$board_number|g" /lab-nfs/koti/lava-apc/power_reset_bootns_titan_$board_number.sh
+sed -i "s|board_number|$board_number|g" /lava/edgeq/lava-apc/power_reset_bootns_titan_$board_number.sh
 
 fi
 
 
 if [[ $board_number  =~ ^2 ]]
 then
-cp /lab-nfs/koti/lava-apc/power_off_on_b0_hawk_generic.sh  /lab-nfs/koti/lava-apc/power_off_on_b0_hawk_$board_number.sh
-cp /lab-nfs/koti/lava-apc/power_off_b0_hawk_generic.sh  /lab-nfs/koti/lava-apc/power_off_b0_hawk_$board_number.sh
-cp /lab-nfs/koti/lava-apc/power_on_b0_hawk_generic.sh   /lab-nfs/koti/lava-apc/power_on_b0_hawk_$board_number.sh
-cp /lab-nfs/koti/lava-apc/boots_b0_hawk_generic.sh  /lab-nfs/koti/lava-apc/boots_b0_hawk_$board_number.sh
-cp /lab-nfs/koti/lava-apc/bootns_b0_hawk_generic.sh /lab-nfs/koti/lava-apc/bootns_b0_hawk_$board_number.sh
-cp /lab-nfs/koti/lava-apc/flash_b0_hawk_generic.sh  /lab-nfs/koti/lava-apc/flash_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/power_off_on_b0_hawk_generic.sh  /lava/edgeq/lava-apc/power_off_on_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/power_off_b0_hawk_generic.sh  /lava/edgeq/lava-apc/power_off_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/power_on_b0_hawk_generic.sh   /lava/edgeq/lava-apc/power_on_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/boots_b0_hawk_generic.sh  /lava/edgeq/lava-apc/boots_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/bootns_b0_hawk_generic.sh /lava/edgeq/lava-apc/bootns_b0_hawk_$board_number.sh
+cp /lava/edgeq/lava-apc/flash_b0_hawk_generic.sh  /lava/edgeq/lava-apc/flash_b0_hawk_$board_number.sh
 
-chmod 777 /lab-nfs/koti/lava-apc/power_*
-chmod 777 /lab-nfs/koti/lava-apc/boots_*
-chmod 777 lab-nfs/koti/lava-apc/bootns_*
-chmod 777 /lab-nfs/koti/lava-apc/flash_*
+chmod 777 /lava/edgeq/lava-apc/power_*
+chmod 777 /lava/edgeq/lava-apc/boots_*
+chmod 777 /lava/edgeq/lava-apc/bootns_*
+chmod 777 /lava/edgeq/lava-apc/flash_*
 
 for ARGUMENT in "$@"
 do
@@ -72,12 +75,12 @@ do
    VALUE="${ARGUMENT:$KEY_LENGTH+1}"
 
    echo "$KEY"="$VALUE"
-   sed -i "s|$KEY|$VALUE|g"  /lab-nfs/koti/lava-apc/power_off_on_b0_hawk_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g"  /lab-nfs/koti/lava-apc/power_off_b0_hawk_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g"  /lab-nfs/koti/lava-apc/power_on_b0_hawk_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g"  /lab-nfs/koti/lava-apc/boots_b0_hawk_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/bootns_b0_hawk_$board_number.sh
-   sed -i "s|$KEY|$VALUE|g" /lab-nfs/koti/lava-apc/flash_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g"  /lava/edgeq/lava-apc/power_off_on_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g"  /lava/edgeq/lava-apc/power_off_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g"  /lava/edgeq/lava-apc/power_on_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g"  /lava/edgeq/lava-apc/boots_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/bootns_b0_hawk_$board_number.sh
+   sed -i "s|$KEY|$VALUE|g" /lava/edgeq/lava-apc/flash_b0_hawk_$board_number.sh
 
 done
 fi
